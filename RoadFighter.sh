f83d4d97a95859407cdcb9a80cd61883d005f4a6
lilian-pfister <129636644+lilian-pfister@users.noreply.github.com>
2026-04-14 01:15:38 +0200

    Create RoadFighter.sh launch script for PortMaster

---
+++ RoadFighter.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# RoadFighter launch script for PortMaster
+# Created on 2026-04-13 23:14:44 UTC
+
+# Insert commands to launch RoadFighter here
+
+# Example command to display a message:
+echo "Launching RoadFighter..."
